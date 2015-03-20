292b7e3b2d405272fa6110916160f2c1c0fb7c00
Daulat <daulat0007@gmail.com>
2015-03-20 23:16:09 +0530

    2nd tym

---
+++ system.py
@@ -0,0 +1,6 @@
+class System:
+    def __init__(self, cores = 5, thread = 50, delay = 0.1 , quantumSize = 1):
+        noOfThread = thread
+        noOfCores = cores
+        switchingDelay = delay
+        quantumSize = quantumSize
