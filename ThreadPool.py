a8c684e3cffa830c284bba3a702686904ecb628a
div1234522 <divyanshu1234522@yahoo.co.in>
2015-03-21 16:05:04 +0530

    Second Draft Done

--- ThreadPool.py
+++ ThreadPool.py
@@ -1,8 +1,12 @@
 class ThreadPool:
+
     def __init__(self, thread , core ,request):
         self.threadId = thread
         self.coreId = core
         self.requestId = request
-    #yet to define
-    def noOfBusyThreads(self):
-        return 0
+    
+    def setNoOfBusyThreads(self, no):
+        self.noOfBusyThreads = no
+
+	def getNoOfBusyThreads(self):
+		return self.noOfBusyThreads 
